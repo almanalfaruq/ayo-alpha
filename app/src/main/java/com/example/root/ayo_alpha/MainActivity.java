0ee59d46dfc5e721650b334d361c913f8c031842
axellageraldinc <axella.gerald@gmail.com>
2016-09-06 13:35:11 +0700

    Edit Desain MockUp
     - Merombak ulang mockup

--- app/src/main/java/com/example/root/ayo_alpha/MainActivity.java
+++ app/src/main/java/com/example/root/ayo_alpha/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.root.ayo_alpha;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home);
+
+        Button gotoaddscreen = (Button) findViewById(R.id.add_button);
+        gotoaddscreen.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent myIntent = new Intent(view.getContext(), add_kegiatan.class);
+                startActivityForResult(myIntent, 0);
+            }
+
+        });
     }
 }
---
+++ app/src/main/java/com/example/root/ayo_alpha/add_kegiatan.java
@@ -0,0 +1,17 @@
+package com.example.root.ayo_alpha;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by axellageraldinc on 9/6/16.
+ */
+public class add_kegiatan extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_kegiatan);
+    }
+}
